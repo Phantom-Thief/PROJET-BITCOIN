12a1ae26f440af176cf2182c36969410c2698db2
Phantom-Thief <jerome.kacimi@free.fr>
2020-11-30 15:04:27 +0100

    Finalisation de la fonction add dans FieldElement en priant pour que l'unitest foire pas (pls)

--- FieldElement.py
+++ FieldElement.py
@@ -107,11 +107,13 @@ class FieldElement(object):
  
 
 class Point(object):
-    def __init__(self,x,y,a,b):
+    def __init__(self,x,y,a,b,Infinity):
         self.a = a
         self.b = b
         self.x = x
         self.y = y
+        self.isInfinity = Infinity
+
         if self.x is None and self.y is None:
             return
         if self.y**2 != self.x**3 + self.a*self.x + self.b   :
@@ -122,11 +124,13 @@ class Point(object):
     def __repr__(self):
         if self.x is None:
             return "Point(infini)"
+        if self.isInfinity == True:
+            return "Point(infini)"
         else:
             return "({}, {})".format(self.x, self.y)
     
     def __eq__(self,other):
-      return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+      return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b and self.isInfinity == other.isInfinity
     
     def __ne__(self,other):
         return not(self == other)
@@ -136,24 +140,40 @@ class Point(object):
             return self
         if self.x is None:
             return other
+        if other.isInfinity == True:
+            return self 
+        if self.isInfinity == True:
+            return other
+
         else:
+
             if self == other:
-                S = (3*self.x + self.a)*((2*self.y).mod_inverse())
-                #S = ((3*self.x + a)*mod_inverse(2*self.y))
+                S = (3*self.x**2 + self.a)*((2*self.y).mod_inverse())
                 X = S**2 - 2*self.x
                 Y = S*(self.x-X)-self.y
-                C = Point(X,Y,self.a,self.b)
+                C = Point(X,Y,self.a,self.b,False)
                 return C
-            else:
+
+            if self.x == other.x and self.y != other.y:
+                inf1= FieldElement(0,self.x.prime)
+                inf2 = FieldElement(0,self.x.prime)
+                return Point(inf1,inf2,self.a,self.b,True)
+
+            if self.x != other.x:
                 S = (other.y-self.y)*((other.x-self.x).mod_inverse())
-                #S =((p2.y-self.y)*mod_inverse(other.x-self.x))
                 X = S**2-self.x-other.x
                 Y = S*(self.x-X)-self.y
-                C = Point(X,Y,self.a,self.b)
+                C = Point(X,Y,self.a,self.b,False)
                 return C  
+            
+            if self == other and self.y == FieldElement(0,self.x.prime):
+                inf1= FieldElement(0,self.x.prime)
+                inf2 = FieldElement(0,self.x.prime)
+                return Point(inf1,inf2,self.a,self.b,True)
+               
 
     def __rmul__(self,val):
-        rmul = Point(None,None,self.a,self.b)
+        rmul = Point(None,None,self.a,self.b,False)
         for i in range(val):
             rmul = self + rmul
         return rmul
@@ -168,11 +188,11 @@ y = FieldElement(1,5)
 a = FieldElement(2,5)
 b = FieldElement(1,5)
 
-p1=Point(x,y,a,b)
+p1=Point(x,y,a,b,False)
 
 x2 = FieldElement(1,5)
 y2 = FieldElement(3,5)
 
-p2=Point(x2,y2,a,b)
+p2=Point(x2,y2,a,b,False)
 p3=p1+p2
 p4 =p1+p3
