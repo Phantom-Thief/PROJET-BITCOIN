fd526a19c5a58f5e8ad1d4b13f4b6f63e79b308d
LouisLeBrozec <74826136+LouisLeBrozec@users.noreply.github.com>
2021-01-18 17:39:19 +0100

    Ajout classe PrivateKey
    Création de signatures

--- S256.py
+++ S256.py
@@ -180,6 +180,20 @@ class S256Field(FieldElement):
         self.num = num
         self.prime = 2**256 - 2**32 - 977
 
+class PrivateKey:
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret * G
+
+    def hex(self):
+        return '{:x}' .format(self.secret).zfill(64)
+
+    def sign(self, z):
+        k = randint(0, N)
+        r = (k * G).x.num
+        s = (z + r*self.secret)/k
+        return Signature(r,s)
+
 class Signature(object):
     def __init__ (self,r,s):
         self.r = r
