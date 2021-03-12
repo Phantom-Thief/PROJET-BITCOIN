9a14ff405779eed3bb1f37b55c543a225963c834
LouisLeBrozec <74826136+LouisLeBrozec@users.noreply.github.com>
2021-03-12 15:08:58 +0100

    Ajout des classes de transaction TX, Tx_In et Tx_out
    Ajout de ces 3 classes et leurs fonction ainsi que l'ajout de 2 fonctions pour lire et encoder des varints dans le helper.py

--- helper.py
+++ helper.py
@@ -55,12 +55,44 @@ def int_to_little_endian(n, length):
     return n.to_bytes(length, 'little')
 
 
-nb1 = b'7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d'
-nb2 = b'ff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c'
-nb3 = b'c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6'
+def read_varint(s):
+    '''read_varint reads a variable integer from a stream''' 
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    else:
+        # anything else is just the integer
+        return i
 
-print("encode")
-print(encode_base58(nb1))
-print(encode_base58(nb2))
-print(encode_base58(nb3))
+
+def encode_varint(i):
+    '''encodes an integer as a varint''' 
+    if i<0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+       return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+       return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+       return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+
+
+
+#nb1 = b'7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d'
+#nb2 = b'ff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c'
+#nb3 = b'c7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6'
+
+#print("encode")
+#print(encode_base58(nb1))
+#print(encode_base58(nb2))
+#print(encode_base58(nb3))
 
