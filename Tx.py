f29b690dba777855c37bb182dd23634783f82ba9
LouisLeBrozec <74826136+LouisLeBrozec@users.noreply.github.com>
2021-03-19 15:16:55 +0100

    Tx.py
    Création de Tx.py qui contiens désormais les classes tx, tx_in et tx_out,  ajours des méthodes parse dans chacune de ces classes

---
+++ Tx.py
@@ -0,0 +1,101 @@
+class Tx:
+    def __init__(self, version, tx_ins, tx_outs, locktime, testnet=False):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+        self.testnet = testnet
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+
+        tx_outs = ''
+        for tx_out in self.tx_outs:
+            tx_outs += tx_out.__repr__() + '\n'
+
+        return 'tx: {}\nversion: {}\ntx_ins:\n{}tx_outs:\n{}locktime: {}'.format(self.id(),self.version, tx_ins, tx_outs, self.locktime,)
+
+    def id(self):
+        ''' version hexadécimale du hash de la transaction'''
+        return self.hash().hex()
+
+    def hash(self):
+        '''hash binaire'''
+        return hash256(self.serialize())[::-1]
+
+    @classmethod
+    def parse(cls, s, testnet=False):
+        version = little_endian_to_int(s.read(4))
+        num_inputs = read_varint(s)
+        inputs = []
+        for _ in range(num_inputs):
+            inputs.append(TxIn.parse(s))
+        num_outputs = read_varint(s)
+        outputs = []
+        for _ in range(num_outputs):
+            outputs.append(TxOut.parse(s))
+        return cls(version, inputs, outputs, None, testnet=testnet)
+
+    
+    def serialize(self):
+        result = int_to_little_endian(self.version, 4)
+        result += encode_varint(len(self.tx_ins))
+        for tx_in in self.tx_ins:
+            result += tx_in.serialize()
+        result += encode_varint(len(self.tx_outs))
+        for tx_out in self.tx_outs:
+            result += tx_out.serialize()
+        result += int_to_little_endian(self.locktime, 4)
+        return result
+
+
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        if script_sig is None:
+            self.script_sig = Script()
+        else:
+            self.script_sig = script_sig
+        self.sequence = sequence
+
+    def __repr__(self):
+        return'{}:{}'.format(self.prev_tx.hex(), self.prev_index,)
+
+    def serialize(self):
+        ''' retourne une version standart en byte de l'output de la transaction'''
+        result = self .prv_tx[::-1]
+        result += int_to_little_endian(self.prev_index, 4)
+        result += self.script_sig.serialize()
+        result += int_to_little_endian(self.sequence, 4)
+        return result
+
+    @classmethod
+    def parse(cls, s):
+        prev_tx = s.read(32)[::-1]
+        prev_index = little_endian_to_int(s.read(4))
+        script_sig = Script.parse(s)
+        sequence = little_endian_to_int(s.read(4))
+        return cls(prev_tx, prev_index, script_sig, sequence)
+
+class TxOut:
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = script_pubkey
+
+    def __repr__(self):
+        return '{}:{}'.format(self.amount, self.script_pubkey)
+
+    def serialize(self):
+        result = int_to_little_endian(self.amount, 8)
+        result += self.script_pubkey.serialize()
+        return result
+
+    @classmethod
+    def parse(cls, s):
+        amount = little_endian_to_int(s.read(8))
+        script_pubkey = Script.parse(s)
+        return cls(amount, script_pubkey)
